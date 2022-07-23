dd02b3224483af9c9181fa141c984a938935101b
berkay <berkay>
2022-07-24 01:23:59 +0200

    halw way through video 3 hour 1.04

--- memories-project/client/src/App.js
+++ memories-project/client/src/App.js
@@ -1,42 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
-
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import memories from './images/memories.png';
-import useStyles from './styles';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import NavBar from './components/NavBar/NavBar';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
 
-    const classes = useStyles();
-    const dispatch = useDispatch();
-    const [currentId, setCurrentId] = useState(0);
-    useEffect(() => {
-      dispatch(getPosts());
-    }, [currentId, dispatch]);
-    
     return (
-        <Container maxWidth="lg">
-          <AppBar className={classes.appBar} position="static" color="inherit">
-            <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-            <img className={classes.image} src={memories} alt="memories" height="60" />
-          </AppBar>
-          <Grow in>
-            <Container>
-                <Grid className={classes.mainContainer} container justify="space between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                    <Posts setCurrentId={setCurrentId} />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                    <Form currentId={currentId} setCurrentId={setCurrentId} />
-                    </Grid>
-                </Grid>
-            </Container>
-          </Grow>
-        </Container>
+        <BrowserRouter>
+          <Container maxWidth="lg">
+            <NavBar />
+            <Routes>
+              <Route path="/" exact element={<Home />} />
+              <Route path="/auth" exact element={<Auth />} />
+            </Routes>
+          </Container>
+        </BrowserRouter>
       );
 }
 
 export default App;
+
+// - use 'Routes' instead of 'Switch'
+// - use 'element={<Home />}' instead of 'component={Home}' prop in 'Route' component
+// - use 'navigate("/")' instead of history.push("/") : should import 'useNavigate' instead of 'useHistory'
